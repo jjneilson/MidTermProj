aad77cf6b0e84b664f7b1b3e61677eb9eeee362f
tstba <tstba@DESKTOP-OCNM9P0>
2021-03-26 23:34:35 -0400

    Added sample test

---
+++ Midterm Coding Project/src/test/java/pkgTestShape/SampleTest.java
@@ -0,0 +1,15 @@
+package pkgTestShape;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		assertEquals(1,1);
+		
+	}
+
+}
